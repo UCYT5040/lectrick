7a26377184bb021d37587be45b8f1e6edb91e6bc
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-07-15 19:46:40 +0200

    put fonts into dedicated folder

--- lectrick/font.py
+++ lectrick/font.py
@@ -2,7 +2,7 @@ from PIL import ImageFont
 
 from fontTools.ttLib import TTFont
 
-FONT_FILES = ["NotoSans-Regular.ttf", "NotoSansSymbols-Regular.ttf", "NotoSansSymbols2-Regular.ttf"]
+FONT_FILES = ["fonts/NotoSans-Regular.ttf", "fonts/NotoSansSymbols-Regular.ttf", "fonts/NotoSansSymbols2-Regular.ttf"]
 FONT_SIZE = 48
 
 # We map fontTools fonts to PIL fonts
