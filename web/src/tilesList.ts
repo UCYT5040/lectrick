2adee1ced1c933752905988dba176587bdc1b5b0
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-07-18 13:38:04 -0500

    Add list of tiles

---
+++ web/src/tilesList.ts
@@ -0,0 +1,35 @@
+import {ClockTile} from "./tiles/clock";
+import {ConditionalHorizontalWireTile} from "./tiles/conditionalHorizontalWire";
+import {CrossWireTile} from "./tiles/crossWire";
+import {DecrementTile} from "./tiles/decrement";
+import {DoubleTile} from "./tiles/double";
+import {HorizontalWireTile} from "./tiles/horizontalWire";
+import {IncrementTile} from "./tiles/increment";
+import {InfinitePowerSourceTile} from "./tiles/infinitePowerSource";
+import {LightBlockTile} from "./tiles/lightBlock";
+import {NotTile} from "./tiles/not";
+import {OneWayLeftTile} from "./tiles/oneWayLeft";
+import {OneWayRightTile} from "./tiles/oneWayRight";
+import {PowerSourceTile} from "./tiles/powerSource";
+import {PressDetector} from "./tiles/pressDetector";
+import {SplitTile} from "./tiles/split";
+import {VerticalWireTile} from "./tiles/verticalWire";
+
+const TILES_LIST = [
+    ClockTile,
+    ConditionalHorizontalWireTile,
+    CrossWireTile,
+    DecrementTile,
+    DoubleTile,
+    HorizontalWireTile,
+    IncrementTile,
+    InfinitePowerSourceTile,
+    LightBlockTile,
+    NotTile,
+    OneWayLeftTile,
+    OneWayRightTile,
+    PowerSourceTile,
+    PressDetector,
+    SplitTile,
+    VerticalWireTile
+];
