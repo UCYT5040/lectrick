228d9eb373fdfa7b652d94e66a031bfd0b61e2ae
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-07-18 13:08:57 -0500

    Add light block

---
+++ web/src/tiles/lightBlock.ts
@@ -0,0 +1,17 @@
+import {OneWayBase} from './oneWayBase';
+
+class LightBlockTile extends OneWayBase {
+    TILE_TYPE = '"LIGHT BLOCK';
+    SAMPLE_CHARS = ["⬛", "⯀", "◼", "▮"];
+
+    constructor(ctx: ExecutionContext, x: number, y: number) {
+        super(ctx, x, y);
+    }
+
+    endTurn() {
+        if (this.currentInput) {
+            const amount = this.currentInput[0];
+            this.ctx.print(String.fromCharCode(amount));
+        }
+    }
+}
