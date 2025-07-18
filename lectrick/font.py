fa87abe6b047ffe4ac3526bb8c989524373160ef
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-07-18 08:19:22 -0500

    Remove accidental space

--- lectrick/font.py
+++ lectrick/font.py
@@ -3,7 +3,7 @@ from PIL import ImageFont
 from fontTools.ttLib import TTFont
 
 FONT_FILES = ["fonts/NotoSans-Regular.ttf", "fonts/NotoSansSymbols-Regular.ttf", "fonts/NotoSansSymbols2-Regular.ttf",
-              "fonts/NotoColorEmoji - Regular.ttf", "fonts/NotoMusic-Regular.ttf",
+              "fonts/NotoColorEmoji-Regular.ttf", "fonts/NotoMusic-Regular.ttf",
               "fonts/NotoSansMath-Regular.ttf", "fonts/NotoSansSignWriting-Regular.ttf",
               ]
 FONT_SIZE = 48
