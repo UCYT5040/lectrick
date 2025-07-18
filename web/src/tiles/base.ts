2f2cee73d3a0c869ff89b7cf355a952314f8c862
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-07-18 11:08:50 -0500

    Add base tile

---
+++ web/src/tiles/base.ts
@@ -0,0 +1,45 @@
+class Tile {
+    TILE_TYPE: string;
+    SAMPLE_CHARS: string[];
+    ctx: ExecutionContext;
+    x: number;
+    y: number;
+    tiles: Tile[];
+    pending_input: any | null;
+    current_input: any | null;
+
+    constructor(ctx: ExecutionContext, x: number, y: number) {
+        this.ctx = ctx;
+        this.x = x;
+        this.y = y;
+        this.pending_input = null;
+        this.current_input = null;
+    }
+
+    equals(other: Tile | [number, number]): boolean {
+        if (Array.isArray(other)) {
+            return this.x === other[0] && this.y === other[1];
+        }
+        return (
+            this.x === other.x &&
+            this.y === other.y &&
+            this.TILE_TYPE === other.TILE_TYPE
+        )
+    }
+
+    acceptEnergy(amount: number, x: number, y: number): void {
+        // To be implemented in subclasses
+    }
+
+    startTurn(): void {
+        // To be implemented in subclasses
+    }
+
+    endTurn(): void {
+        // To be implemented in subclasses
+    }
+
+    advanceTurn(): void {
+        // To be implemented in subclasses
+    }
+}
