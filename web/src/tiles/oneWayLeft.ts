95f3c8a5087c20f7554f5e9cbb6853444d461df2
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-07-18 13:24:54 -0500

    Add one way left and right

---
+++ web/src/tiles/oneWayLeft.ts
@@ -0,0 +1,32 @@
+import {Tile} from './base';
+
+class OneWayLeftTile extends Tile {
+    TILE_TYPE = 'ONE WAY LEFT';
+    SAMPLE_CHARS = ["<"];
+
+    constructor(ctx: ExecutionContext, x: number, y: number) {
+        super(ctx, x, y);
+    }
+
+    acceptEnergy(amount: number, x: number, y: number) {
+        if (y !== this.y || x >= this.x) {
+            return false; // Only accept energy from the right
+        }
+        self.pendingInput = amount;
+    }
+
+    advanceTurn() {
+        self.currentInput = self.pendingInput;
+        self.pendingInput = null;
+    }
+
+    startTurn() {
+        if (!self.currentInput) {
+            return
+        }
+        const targetTile = this.ctx.getTile(this.x - 1, this.y);
+        if (targetTile) {
+            targetTile.acceptEnergy(this.currentInput, this.x, this.y);
+        }
+    }
+}
---
+++ web/src/tiles/oneWayRight.ts
@@ -0,0 +1,32 @@
+import {Tile} from './base';
+
+class OneWayRightTile extends Tile {
+    TILE_TYPE = 'ONE WAY RIGHT';
+    SAMPLE_CHARS = [">"];
+
+    constructor(ctx: ExecutionContext, x: number, y: number) {
+        super(ctx, x, y);
+    }
+
+    acceptEnergy(amount: number, x: number, y: number) {
+        if (y !== this.y || x <= this.x) {
+            return false; // Only accept energy from the left
+        }
+        self.pendingInput = amount;
+    }
+
+    advanceTurn() {
+        self.currentInput = self.pendingInput;
+        self.pendingInput = null;
+    }
+
+    startTurn() {
+        if (!self.currentInput) {
+            return
+        }
+        const targetTile = this.ctx.getTile(this.x + 1, this.y);
+        if (targetTile) {
+            targetTile.acceptEnergy(this.currentInput, this.x, this.y);
+        }
+    }
+}
