f21a1f3e85ddf5d7a7c068bc39e4c572c5995c64
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-07-18 13:00:26 -0500

    Add horizontal and vertical wire tiles

---
+++ web/src/tiles/horizontalWire.ts
@@ -0,0 +1,33 @@
+import {OneWayBase} from './oneWayBase';
+
+class HorizontalWireTile extends OneWayBase {
+    TILE_TYPE = 'HORIZONTAL WIRE';
+    SAMPLE_CHARS = ["-", "‐", "‒"];
+
+    constructor(ctx: ExecutionContext, x: number, y: number) {
+        super(ctx, x, y);
+    }
+
+    acceptanceFilter(x: number, y: number): boolean {
+        return y === this.y
+    }
+
+    startTurn() {
+        if (!this.currentInput) {
+            return;
+        }
+        const [amount, x, y] = this.currentInput;
+        let dx;
+        if (x < this.x) {
+            dx = -1; // Left
+        } else if (x > this.x) {
+            dx = 1; // Right
+        } else {
+            return; // No horizontal movement
+        }
+        const targetTile = this.ctx.getTile(this.x + dx, this.y);
+        if (targetTile) {
+            targetTile.acceptEnergy(amount, this.x, this.y);
+        }
+    }
+}
---
+++ web/src/tiles/verticalWire.ts
@@ -0,0 +1,33 @@
+import {OneWayBase} from './oneWayBase';
+
+class VerticalWireTile extends OneWayBase {
+    TILE_TYPE = 'VERTICAL WIRE';
+    SAMPLE_CHARS = ["|"];
+
+    constructor(ctx: ExecutionContext, x: number, y: number) {
+        super(ctx, x, y);
+    }
+
+    acceptanceFilter(x: number, y: number): boolean {
+        return x === this.x
+    }
+
+    startTurn() {
+        if (!this.currentInput) {
+            return;
+        }
+        const [amount, x, y] = this.currentInput;
+        let dy;
+        if (y < this.y) {
+            dy = -1; // Left
+        } else if (y > this.y) {
+            dy = 1; // Right
+        } else {
+            return; // No horizontal movement
+        }
+        const targetTile = this.ctx.getTile(this.x, this.y + dy);
+        if (targetTile) {
+            targetTile.acceptEnergy(amount, this.x, this.y);
+        }
+    }
+}
