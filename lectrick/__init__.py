17bdf61ad5b75db233bb9542e069c9f48bf3a339
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-07-24 15:50:37 +0200

    Export main commands in __init__.py for package-level access.

---
+++ lectrick/__init__.py
@@ -0,0 +1,14 @@
+from .__main__ import lookup_character, map_command_impl
+from .__main__ import generate_overlays_ as generate_overlays
+from .__main__ import generate_shapes_ as generate_shapes
+from .__main__ import reverse_lookup_ as reverse_lookup
+from .__main__ import run_program_ as run_program
+
+__all__ = [
+    "lookup_character",
+    "map_command_impl",
+    "generate_overlays",
+    "generate_shapes",
+    "reverse_lookup",
+    "run_program",
+]
